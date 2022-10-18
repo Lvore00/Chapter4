f178924192657e951905c871fcc2baf75b920c26
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-18 18:22:51 -0400

    Update IllegalTriangleSideException.java

--- Chapter4/IllegalTriangleSideException.java
+++ Chapter4/IllegalTriangleSideException.java
@@ -1,12 +1,13 @@
 package Chapter4;
 
 
-public class IllegalTriangleSideException extends Exception{
-    public IllegalTriangleSideException(){
-        super ("Illegal Triangle Side Exception occurred");
+public class IllegalTriangleSideException extends Exception {
+
+    public IllegalTriangleSideException() {
+        super("Illegal Triangle Side Exception occurred");
     }
 
-    public IllegalTriangleSideException(String msg){
-        super(msg);
+    public IllegalTriangleSideException(String message) {
+        super(message);
     }
 }
