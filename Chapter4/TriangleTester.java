3608a9a06edf740b9ceb60b6a8d2d9c09cf28abd
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-18 18:25:33 -0400

    Update TriangleTester.java

--- Chapter4/TriangleTester.java
+++ Chapter4/TriangleTester.java
@@ -1,16 +1,44 @@
 package Chapter4;
 
 
-//public class TriangleTester {
-//    public static void main(String[] args) throws IllegalTriangleSideException{
-//
-//        try{
-//            Triangle t1 = new Triangle(4, 5, 6);
-//            Triangle t2 = new Triangle(5, 7, 8);
-//            Triangle t3 = new Triangle(8, 4, 1);
-//
-//    }catch(IllegalTriangleSideException e){
-//            System.out.println(e);
-//        }
-//        System.out.println(Triangle.getTotalNumber());
-//}
+public class TriangleTester {
+    public static void main(String[] args) throws IllegalTriangleSideException {
+
+        try {
+            System.out.println();
+            System.out.println("Testing Triangle()");
+            Triangle triangle = new Triangle();
+            System.out.println("Triangle() is valid");
+        } catch (IllegalTriangleSideException itse) {
+            System.out.println(itse.getMessage());
+        }
+
+        try {
+            System.out.println();
+            System.out.println("Testing Triangle (4.1, 3.6, 7.9)");
+            Triangle triangle = new Triangle(4.1, 3.6, 7.9);
+            System.out.println("Triangle (4.1, 3.6, 7.9) is valid");
+        } catch (IllegalTriangleSideException itse) {
+            System.out.println(itse.getMessage());
+        }
+
+        try {
+            System.out.println();
+            System.out.println("Testing Triangle (2.3, 9.0, 5.5)");
+            Triangle triangle = new Triangle(2.3, 9.0, 5.5);
+            System.out.println("Triangle (2.3, 9.0, 5.5) is valid");
+        } catch (IllegalTriangleSideException itse) {
+            System.out.println(itse.getMessage());
+        }
+
+        try {
+            System.out.println();
+            System.out.println("Testing Triangle (1.3, 4.5, 4.5)");
+            Triangle triangle = new Triangle(1.3, 4.5, 4.5);
+            System.out.println("Triangle (1.3, 4.5, 4.5) is valid");
+        } catch (IllegalTriangleSideException itse) {
+            System.out.println(itse.getMessage());
+        }
+    }
+
+}
